7aab3bb2e16892a125ba390195a0d7023bb1a88f
wprumizaca <wperezrumizaca@gmail.com>
2023-04-26 13:59:17 +0200

    Microservicio inical de Registro de Usuarios

---
+++ src/main/java/com/microservicio/registrarUsuario/RegistrarUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.registrarUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarUsuarioApplication.class, args);
+	}
+
+}
