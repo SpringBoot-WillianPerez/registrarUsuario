9f282216962f29fedaa51d3dd5609e549339c01f
wprumizaca <wperezrumizaca@gmail.com>
2023-04-26 18:04:01 +0200

    Ultimas modificaciones y guardamos

--- src/main/java/com/microservicio/registrarUsuario/persistence/repository/UserRepository.java
+++ src/main/java/com/microservicio/registrarUsuario/persistence/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.microservicio.registrarUsuario.persistence.repository;
 import com.microservicio.registrarUsuario.persistence.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
     Optional<User> findByUsername(String username);
 }
