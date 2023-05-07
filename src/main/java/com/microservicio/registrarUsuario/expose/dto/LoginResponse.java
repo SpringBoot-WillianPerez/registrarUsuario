1b26237a2529571b96ca7ce94161ae8dbae8776d
wprumizaca <wperezrumizaca@gmail.com>
2023-05-07 15:11:24 +0200

    record LoginResponse antes de cambiar a class

--- src/main/java/com/microservicio/registrarUsuario/expose/dto/LoginResponse.java
+++ src/main/java/com/microservicio/registrarUsuario/expose/dto/LoginResponse.java
@@ -1,7 +1,13 @@
 package com.microservicio.registrarUsuario.expose.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
+
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public record LoginResponse(String username, List<String> roles, String token, String refreshToken) {
+
 
 }
--- src/main/java/com/microservicio/registrarUsuario/persistence/repository/RefreshTokenRepository.java
+++ src/main/java/com/microservicio/registrarUsuario/persistence/repository/RefreshTokenRepository.java
@@ -11,6 +11,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, User
 
     Optional<RefreshToken> findByToken(String token);
 
-    @Modifying
+    @Modifying //para actualizacion y borrado
     int deleteByUser(User user); //Borrar un token introduciendo usuario. No se va a usar de momento
 }
